9250c9425de84b57c9815650471b1c1546283ba1
Suvankar Mazumder <smazumder@gmail.com>
2017-04-26 16:38:03 -0400

    Update smshell.c

--- src/smshell.c
+++ src/smshell.c
@@ -12,15 +12,71 @@
  **/
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+
 
 //Buffer size for read line
 #define SMSHELL_BUFFER_SIZE 1024;
+#define EXIT_STATUS 0;
+#define SMSHELL_TOK_DELIM " \t\r\n\a"
 
-#define EXIT_STATUS  0;
+//*********************** Inbuilt shell commands *****************************
+int smsh_cd(char **args);
+int smsh_help(char **args);
+int smsh_exit(char **args);
 
-//Not implemented for now
-void  load_config() {}
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
 
+int (*builtin_func[]) (char **) = {
+  &smsh_cd,
+  &smsh_help,
+  &smsh_exit
+};
+
+int smsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+
+int smsh_cd(char **args) {
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+int smsh_help(char **args) {
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (int i = 0; i < smsh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int smsh_exit(char **args) {
+    return 0;
+}
+
+//****************************************** End of shell commands  implementation
 /**
  * Allocates a block (see buffer size) and then reads a line from stdin.
  * If  text entered exceeds the initial block size then it reallocates more space.
@@ -58,6 +114,87 @@ char *smshell_read_line(void) {
     }
 
 }
+
+/**
+ * Does not allow quoting or backslash escaping in command line arguments.
+ *
+ */
+char **smshell_parse_line(char *line) {
+   int bufSize = SMSHELL_BUFFER_SIZE;
+   int pos = 0;
+   char **tokens;
+   char *token;
+
+   tokens = malloc(bufSize * sizeof(char*));
+   if (!tokens) {
+        fprintf(stderr, "smshell: failed to allocate enough memory for shell.\n");
+         exit(EXIT_FAILURE);
+   }
+   token = strtok(line,SMSHELL_TOK_DELIM);
+   while(token != NULL) {
+       tokens[pos] = token;
+       pos++;
+
+       if(pos >= bufSize) {
+           bufSize += SMSHELL_BUFFER_SIZE;
+           tokens =realloc(tokens, bufSize * sizeof(char*));
+           if(!tokens) {
+             fprintf(stderr, "smshell: failed to allocate enough memory for shell.\n");
+             exit(EXIT_FAILURE);
+           }
+       }
+       token = strtok(NULL,SMSHELL_TOK_DELIM);
+   }
+   tokens[pos] = NULL;
+   return tokens;
+
+}
+/**
+ *  In order to execute a command, fork a new process and if it succeeds
+ *  run execvp  that  creates that executes the command as a child process.
+ *
+ */
+int  smshell_launch(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if(pid == 0) {
+        //run execvp as a child process.
+        if(execvp(args[0],args) == -1) {
+            perror("sm-shell, error executing the shell command.");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        //Error forking
+        perror("sm-shell- error forking.");
+    }else {
+        do {
+            wpid = waitpid(pid,&status,WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+   return 1;
+}
+
+int smshell_execute(char **args){
+
+    //Empty command was entered.
+    if(args[0] == NULL)
+        return 1;
+    //Otherwise check whether build-in shell functions needs to  be called.
+    for(int i=0; i < smsh_num_builtins(); i++) {
+      if (strcmp(args[0], builtin_str[i]) == 0) {
+          return (*builtin_func[i])(args);
+      }
+    }
+    //finally run execute if the command passed is not from built-in funcs
+    return smshell_launch(args);
+
+}
+
+void perform_cleanup(){
+    //To be implemented
+}
 /**
  * The basic loop of the shell program will
  *  1. read the command from stdin
@@ -72,7 +209,7 @@ void smshell_loop() {
     do {
         printf("<smshell> ");
         line = smshell_read_line();
-        args = smshell_parse_line();
+        args = smshell_parse_line(line);
         status = smshell_execute(args);
 
         free(line);
@@ -81,9 +218,17 @@ void smshell_loop() {
     }while (status);
 }
 
+void startup_msg() {
+    printf("*********************************************\n");
+    printf("*** Welcome to smshell.                   ***\n");
+    printf("*********************************************\n");
+    printf("*********************************************\n");
+    printf("*********************************************\n");
+}
 int main(int argc, char **argv) {
     //Load any configuration file
-    load_config();
+    //load_config();
+    startup_msg();
     //Run the main shell loop
     smshell_loop();
     //Perform any clean ups before exiting the shell.
